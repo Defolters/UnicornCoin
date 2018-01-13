6c302fa0d04bfd594061eefc334eb12ae8dc7a52
Defolters <defolterrik@gmail.com>
2018-01-13 23:15:15 +0300

    address

--- src/keygenerator.h
+++ src/keygenerator.h
@@ -21,8 +21,8 @@ private:
 #endif // KEYGENERATOR_H
 
 /*
- *
- * privateKey = ''.join(['%x' % random.randrange(16) for x in range(0, 64)])
+ * https://royalforkblog.github.io/2014/08/11/graphical-address-generator/#hello
+ * privateKey = ''.join(['%x' % random.randrange(16) for x in range(0, 64)]) or hash256(passphrase)
  * secretKey = ecdsa.SigningKey.from_string(privateKey.decode('hex'), curve=ecdsa.SECP256k1)
  * verifyingKey = secretKey.VerifyingKey.toString().encode('hex')
  * publicKey = verifyingKey
